d9d9dc3eb9896c0135cf64c55351d085af9d4b1f
sylvan452 <sylvan452@gmail.com>
2023-03-08 14:11:01 +0100

    done

---
+++ README.m
@@ -0,0 +1,3 @@
+This is the Part I of the AirBnB project. We worked on the backend of the project, using the console as an interface with the help of python's cmd module.
+
+The python objects(data) generated were stored in a json file which is to be accessed wth the help of python's jason module.
