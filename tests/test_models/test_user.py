77995751740086f3db85b9307439351a96d9b7cb
Abdulbasit <aabdulrafiu721@gmail.com>
2023-03-12 11:33:56 +0000

    chceck for attributes in user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -39,3 +39,15 @@ class TestUser(unittest.TestCase):
 
     def test_for_functions(self):
         self.assertIsNotNone(User.__doc__)
+
+    def test_attribute_exist(self):
+        self.assertTrue("email" in self.my_user.__dict__)
+        self.assertTrue("id" in self.my_user.__dict__)
+        self.assertTrue("created_at" in self.my_user.__dict__)
+        self.assertTrue("created_at" in self.my_user.__dict__)
+        self.assertTrue("password" in self.my_user.__dict__)
+        self.assertTrue("updated_at" in self.my_user.__dict__)
+        self.assertTrue("first_name" in self.my_user.__dict__)
+        self.assertTrue("last_name" in self.my_user.__dict__)
+
+
