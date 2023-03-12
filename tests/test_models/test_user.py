ebcb563193b2c3dfe2663099de85cee17f912e48
Abdulbasit <aabdulrafiu721@gmail.com>
2023-03-12 11:14:24 +0000

    writing test, test for user

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import unittest
+from models.user import User
+from models.base_model import BaseModel
+import os
+import pycodestyle
+
+
+class TestUser(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        """ fills user with data to test with at the start of the test """
+        cls.my_user = User()
+        cls.my_user.first_name = "User1"
+        cls.my_user.last_name = "Alx"
+        cls.my_user.email = "user1@alx.com"
+        cls.my_user.password = "root"
+
+    @classmethod
+    def tearDownClass(cls):
+        """ removes all data created during test and deletes file.json if
+            exists
+        """
+        del cls.my_user
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+
+    def test_conformance(self):
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['models/user.py'])
+        self.assertEqual(result.total_errors, 0, "Fix pep8")
+    
+    def test_is_subclass(self):
+        self.assertTrue(issubclass(self.my_user.__class__, BaseModel), True)
