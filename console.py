9b06726c116ac691eafc6d3357841ce6f4b274f7
Abdulbasit <aabdulrafiu721@gmail.com>
2023-03-13 12:44:40 +0000

    show on its way

--- console.py
+++ console.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 """Console for Airbnb clone """
 import cmd
+from models import storage
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.city import City
 
 
 class HBNBCommand(cmd.Cmd):
     """ Entry into the console"""
     prompt = "(hbnb)"
+    classes = {"BaseModel", "Amenity", "User", "Place", "Review", "State",
+               "City"}
 
     def do_EOF(self, line):
         """indicates end of line, exits on ctrl D"""
@@ -19,6 +29,27 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """executes when an empty line is passed"""
         pass
+
+    def do_create(self, line):
+        """creates an instance of BaseModel, line contains the class"""
+        if len(line) == 0:
+            print("** class name missing **")
+        elif line not in self.classes:
+            print("** class doesn't exist **")
+        else:
+            '''evaluate the string in line to a funtion object'''
+            instance = eval(line)()
+            instance.save()
+            print(instance.id)
+
+    def do_show(self, *args):
+        '''prints the string representation of an instance: name.id'''
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if args[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
 
 
 if __name__ == "__main__":
